765067a4abb67f954a442c7ab2c70aa330010afb
Ashish Anand N <ashishmax31@gmail.com>
2020-08-09 02:19:00 +0530

    refactor employee-db program; Add unit-test cases

--- employee-db/src/employee_terminal/employee_operations.rs
+++ employee-db/src/employee_terminal/employee_operations.rs
@@ -1,59 +1,108 @@
 use std::collections::HashMap;
+use std::vec::Vec;
 
+#[derive(Debug, PartialEq)]
 pub struct EmployeeDb {
-  db: HashMap<String, String>,
+    db: HashMap<String, Vec<String>>,
 }
-
 
 pub fn new_employee_db() -> EmployeeDb {
-  EmployeeDb{
-    db: HashMap::new()
-  }
+    EmployeeDb { db: HashMap::new() }
 }
 
+impl EmployeeDb {
+    pub fn add_employee(&mut self, name: &str, dept: &str) {
+        match self.db.get_mut(dept) {
+            Some(employees) => {
+                employees.push(name.to_string());
+                employees.sort();
+            }
+            None => {
+                self.db.insert(dept.to_string(), vec![name.to_string()]);
+            }
+        }
+    }
 
-impl EmployeeDb{
-  pub fn add_employee(&mut self, name: &str, dept: &str) -> Option<String> {
-    self.db.insert(name.to_string(), dept.to_string())
-  }
+    pub fn get_employees_by_dept(&self, dept: &str) -> Vec<&String> {
+        match self.db.get(dept) {
+            Some(items) => items.iter().collect(),
+            None => Vec::new(),
+        }
+    }
 
-  pub fn get_employees_by_dept(&self, dept: &str) -> Vec<&String>{
-    self.db.iter().filter_map(|(k, v)|{
-      if v == dept {
-        Some(k)
-      }else {
-        None
-      }
-    }).collect()
-  }
-
-  pub fn list_employees(&self) -> Vec<String> {
-    let depts = get_sorted_dept_names(self);
-    depts.iter().flat_map(|dept| {
-      build_employee_listing(get_sorted_employees_by_dept(self, dept), dept)
-    }).collect()
-  }
+    pub fn list_employees(&self) -> Vec<String> {
+        self.db
+            .iter()
+            .flat_map(|(dept, employees)| build_employee_listing(employees, dept))
+            .collect()
+    }
 }
 
-fn get_sorted_dept_names(db_obj: &EmployeeDb) -> Vec<&String> {
-  let mut values: Vec<&String> = db_obj.db.values().collect();
-  values.sort();
-  values.dedup();
-  values
+fn build_employee_listing(employees: &Vec<String>, dept: &str) -> Vec<String> {
+    employees
+        .iter()
+        .map(|employee| format!("Name: {} Department: {}", employee, dept))
+        .collect()
 }
 
-fn get_sorted_employees_by_dept(db_obj: &EmployeeDb, dept: &str) -> Vec<String> {
-  let mut employees: Vec<String> = db_obj.get_employees_by_dept(dept)
-                                                        .iter()
-                                                        .map(|x| String::from(*x))
-                                                        .collect();
-  employees.sort();
-  employees
-}
+mod testing {
+    use super::*;
 
-fn build_employee_listing(employees: Vec<String>, dept: &str) -> Vec<String> {
-  employees.iter().map(|employee|{
-    format!("Name: {} Department: {}", employee, dept)
-  }).collect()
-}
+    #[test]
+    fn test_add_employee() {
+        let mut test_db = new_employee_db();
+        test_db.add_employee("batman", "gotham");
+        test_db.add_employee("superman", "daily planet");
+        test_db.add_employee("robin", "gotham");
 
+        let mut stored_depts: Vec<&String> = test_db.db.keys().collect();
+        stored_depts.sort();
+        assert_eq!(stored_depts, ["daily planet", "gotham"]);
+
+        let mut stored_employees: Vec<&String> = test_db.db.values().flatten().collect();
+        stored_employees.sort();
+        assert_eq!(stored_employees, ["batman", "robin", "superman"]);
+    }
+
+    #[test]
+    fn test_get_employees_by_dept() {
+        let mut test_db = new_employee_db();
+        test_db.add_employee("robin", "gotham");
+        test_db.add_employee("superman", "daily planet");
+        test_db.add_employee("batman", "gotham");
+        test_db.add_employee("Lois lane", "daily planet");
+
+        let res_1 = test_db.get_employees_by_dept("gotham");
+        // Assert names are stored in the sorted order.
+        assert_eq!(res_1, ["batman", "robin"]);
+
+        let res_2 = test_db.get_employees_by_dept("daily planet");
+        assert_eq!(res_2, ["Lois lane", "superman"]);
+
+        assert_eq!(
+            test_db.get_employees_by_dept("Area 51"),
+            Vec::<&String>::new()
+        );
+    }
+
+    #[test]
+    fn test_list_employees() {
+        let mut test_db = new_employee_db();
+        test_db.add_employee("robin", "gotham");
+        test_db.add_employee("superman", "daily planet");
+        test_db.add_employee("batman", "gotham");
+        test_db.add_employee("Lois lane", "daily planet");
+
+        let mut res = test_db.list_employees();
+        res.sort();
+
+        let mut expected_result = [
+            "Name: Lois lane Department: daily planet",
+            "Name: superman Department: daily planet",
+            "Name: batman Department: gotham",
+            "Name: robin Department: gotham",
+        ];
+        expected_result.sort();
+        assert_eq!(res, expected_result);
+    }
+}
--- employee-db/src/employee_terminal/terminal.rs
+++ employee-db/src/employee_terminal/terminal.rs
@@ -2,90 +2,152 @@ use crate::employee_terminal::employee_operations;
 use std::io;
 use std::process;
 
-enum Command {
-  AddUser(String, String),
-  DeptPeople(String),
-  ListPeople,
-  Exit,
-  Error
+#[derive(Debug, PartialEq)]
+enum Command<'a> {
+    AddUser(&'a str, &'a str),
+    DeptPeople(&'a str),
+    ListPeople,
+    Exit,
+    Error(&'static str),
 }
 
-pub fn start_terminal(){
-  let mut db = employee_operations::new_employee_db();
-  println!("Welcome to the employee database terminal!");
-  println!("Available commands are:");
-  println!("1) add <name> to <dept.name>");
-  println!("2) show people in <dept.name>");
-  println!("3) list all");
-  println!("Type 'exit' at any point to exit the terminal");
+pub fn start_terminal() {
+    let mut db = employee_operations::new_employee_db();
+    println!("Welcome to the employee database terminal!");
+    println!("Available commands are:");
+    println!("1) add <name> to <dept.name>");
+    println!("2) show people in <dept.name>");
+    println!("3) list all");
+    println!("Type 'exit' at any point to exit the terminal");
 
-  loop {
-    println!();
-    let user_input = get_user_input();
-    match command(user_input){
-      Command::AddUser(name, dept) => add_user(&mut db, name, dept),
-      Command::DeptPeople(dept) => show_people_in_dept(&mut db, dept),
-      Command::ListPeople => list_people(&mut db),
-      Command::Error => panic!("Unknown action"),
-      Command::Exit => process::exit(0),
+    loop {
+        let user_input = get_user_input();
+        match command(&user_input) {
+            Ok(prepared_command) => match prepared_command {
+                Command::AddUser(name, dept) => add_user(&mut db, name, dept),
+                Command::DeptPeople(dept) => show_people_in_dept(&db, dept),
+                Command::ListPeople => list_people(&db),
+                Command::Exit => process::exit(0),
+                Command::Error(msg) => handle_error(msg),
+            },
+            Err(msg) => handle_error(msg),
+        }
     }
-  }
 }
 
-
-fn get_user_input() -> String{
-  let mut input = String::new();
-  io::stdin()
-      .read_line(&mut input)
-      .expect("Expected a line of text");
-  String::from(input.trim())
+fn handle_error(msg: &str) {
+    eprintln!("{}", msg);
+    process::exit(1);
 }
 
-fn command(input: String) -> Command {
-  let words: Vec<&str> = input.split_whitespace().collect();
-  match words[0]{
-    "add"  => build_add_command(words),
-    "show" => build_show_command(words),
-    "list" => Command::ListPeople,
-    "exit" => Command::Exit,
-    _ => Command::Error,
-  }
+fn get_user_input() -> String {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Expected a line of text");
+    input.trim().to_owned()
 }
 
-fn build_add_command(words: Vec<&str>) -> Command {
-  if words.len() == 4 {
-    Command::AddUser(
-      String::from(words[1]),
-      String::from(words[3]),
-    )
-  }else {
-    Command::Error
-  }
+fn command<'a>(input: &'a str) -> Result<Command<'a>, &'static str> {
+    let mut words = input.split_whitespace();
+    match words.next() {
+        Some(first_commmand) => match first_commmand {
+            "add" => Ok(build_add_command(words)),
+            "show" => Ok(build_show_command(words)),
+            "list" => Ok(Command::ListPeople),
+            "exit" => Ok(Command::Exit),
+            _ => Err("Unsupported action!"),
+        },
+        None => Err("Expected at least one word input"),
+    }
 }
 
-fn build_show_command(words: Vec<&str>) -> Command {
-  if words.len() == 4 {
-    Command::DeptPeople(
-      String::from(words[3]),
-    )
-  }else {
-    Command::Error
-  }
+fn build_add_command<'a>(words: std::str::SplitWhitespace<'a>) -> Command<'a> {
+    let words: Vec<_> = words.collect();
+    if words.len() < 3 {
+        Command::Error("Incorrect command")
+    } else {
+        Command::AddUser(words[0], words[2])
+    }
 }
 
-fn add_user(db: &mut employee_operations::EmployeeDb, name: String, dept: String){
-  db.add_employee(&name[..], &dept[..]);
+fn build_show_command<'a>(words: std::str::SplitWhitespace<'a>) -> Command<'a> {
+    let words: Vec<_> = words.collect();
+
+    if words.len() < 3 {
+        Command::Error("Incorrect command")
+    } else {
+        Command::DeptPeople(words[2])
+    }
 }
 
-fn show_people_in_dept(db: &mut employee_operations::EmployeeDb, dept: String) {
-  let result = db.get_employees_by_dept(&dept[..]);
-  for name in result {
-    println!("{}", name);
-  }
+// Impure fns :(
+fn add_user(db: &mut employee_operations::EmployeeDb, name: &str, dept: &str) {
+    db.add_employee(name, dept);
 }
-fn list_people(db: &mut employee_operations::EmployeeDb){
-  let result = db.list_employees();
-  for emp_details in result {
-    println!("{}", emp_details);
-  }
+
+fn show_people_in_dept(db: &employee_operations::EmployeeDb, dept: &str) {
+    let result = db.get_employees_by_dept(dept);
+    for name in result {
+        println!("{}", name);
+    }
+}
+fn list_people(db: &employee_operations::EmployeeDb) {
+    let result = db.list_employees();
+    for emp_details in result {
+        println!("{}", emp_details);
+    }
+}
+
+mod test {
+    use super::*;
+    #[test]
+    fn test_build_show_command() {
+        let command_1 = "hello world";
+        assert_eq!(
+            build_show_command(command_1.split_whitespace()),
+            Command::Error("Incorrect command")
+        );
+
+        let command_2 = "employees in software";
+        assert_eq!(
+            build_show_command(command_2.split_whitespace()),
+            Command::DeptPeople("software")
+        );
+    }
+
+    #[test]
+    fn test_build_add_command() {
+        let command_1 = "batman to gotham";
+        assert_eq!(
+            build_add_command(command_1.split_whitespace()),
+            Command::AddUser("batman", "gotham")
+        );
+
+        let command_2 = "robin to";
+        assert_eq!(
+            build_add_command(command_2.split_whitespace()),
+            Command::Error("Incorrect command")
+        );
+    }
+    #[test]
+    fn test_command() {
+        let input_1 = "";
+        assert_eq!(command(input_1), Err("Expected at least one word input"));
+
+        let input_2 = "add batman to gotham";
+        assert_eq!(command(input_2), Ok(Command::AddUser("batman", "gotham")));
+
+        let input_3 = "list all";
+        assert_eq!(command(input_3), Ok(Command::ListPeople));
+
+        let input_4 = "show people in gotham";
+        assert_eq!(command(input_4), Ok(Command::DeptPeople("gotham")));
+
+        let input_5 = "exit 123";
+        assert_eq!(command(input_5), Ok(Command::Exit));
+
+        let input_6 = "this is an unsupported command";
+        assert_eq!(command(input_6), Err("Unsupported action!"));
+    }
 }
